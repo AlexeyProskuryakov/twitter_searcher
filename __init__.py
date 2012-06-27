2ce2f8022f7fe64fe0b0c0b3604e19215611b3b8
Alexey Proskuryakov <alexey.proskuryakov@gmail.com>
2012-06-28 05:02:19 +0700

    good_start

---
+++ __init__.py
@@ -0,0 +1,39 @@
+import re
+import os
+
+from pip.commands.install import InstallCommand
+InstallCommand
+
+
+__author__ = 'Alesha'
+path_name = os.path.join(os.path.dirname(__file__)).replace('\\', '/')
+
+
+def foo():
+    f = open(path_name + '/serch_engine/commands.py'.replace('\\', '/'))
+    lines = f.readlines()
+    out_lines = []
+    for line in lines:
+        if line[0] == '#':
+            out_lines.append(line+'\n')
+        elif len(line):
+            strings = re.compile('[a-zA-Z0-9/]+').findall(line)
+            print strings
+            if len(strings) > 5:
+                method = strings[0]
+                name = strings[1]
+                description = strings[2:]
+                op_name = '_'.join(entity for entity in re.compile('[a-z]+').findall(name))
+                comment = method + ',' + ' '.join(description)
+                code = op_name + ' = ' + "'" + name + "'"
+                out_lines.append("#"+comment+'\n')
+                out_lines.append(code+'\n')
+    for line in out_lines:
+        print line
+
+    f.close()
+    g = open(path_name + '/serch_engine/commands_.py'.replace('\\', '/'),'w+')
+    g.writelines(out_lines)
+    g.close()
+if __name__ == '__main__':
+    foo()
