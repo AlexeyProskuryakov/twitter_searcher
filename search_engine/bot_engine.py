f4e9398d30afbfaf7e3cce3ad2530824bc4d7c92
Alexey Proskuryakov <alexey.proskuryakov@gmail.com>
2012-07-05 04:51:40 +0700

    may be end

--- search_engine/bot_engine.py
+++ search_engine/bot_engine.py
@@ -1,7 +1,10 @@
 #-*- coding: utf-8 -*-
 import webbrowser
 import  tweepy
-from tweepy.models import User
+from model.db import db_handler
+
+from model.functions import get_statistic_of
+from model.tw_model import user
 
 __author__ = 'Alesha'
 
@@ -24,47 +27,82 @@ def init_account():
     return auth.access_token.key, auth.access_token.secret
 
 
-class bot:
-    def __init__(self, inited=False):
+class engine:
+    def __init__(self, inited=False, db_handler=db_handler()):
         self.auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-        if inited:
+        if not inited:
             self.access_token, self.access_token_secret = init_account()
             self.auth.set_access_token(self.access_token, self.access_token_secret)
         else:
             self.auth.set_access_token(access_token, access_token_secret)
 
         self.__init_api()
+        self.db = db_handler
 
     def __init_api(self):
         self.api = tweepy.API(self.auth)
-        self.folowers = [{'name': user.screen_name, 'obj': user} for user in tweepy.Cursor(self.api.followers).items()]
-        self.friends = [{'name': user.screen_name, 'obj': user} for user in tweepy.Cursor(self.api.friends).items()]
-        self.timeline = [{'text': status.text, 'obj': status} for status in tweepy.Cursor(self.api.home_timeline).items(
-            max_time_line)]
 
-    def map_reduce(self, what, map=lambda what:what['obj'].friends(),
-                   reduce=lambda user:{'name': user.screen_name, 'obj': user}):
+    #        self.folowers = [{'name': user.screen_name, 'obj': user} for user in tweepy.Cursor(self.api.followers).items()]
+    #        self.friends = [{'name': user.screen_name, 'obj': user} for user in tweepy.Cursor(self.api.friends).items()]
+    #        self.timeline = [{'text': status.text, 'obj': status} for status in tweepy.Cursor(self.api.home_timeline).items(
+    #            max_time_line)]
+
+    def flush(self, data, name=lambda x:x.screen_name):
+        return [name(element) for element in data]
+
+    def get_user_by_name(self, user_name):
+        return self.api.get_user(user_name)
+
+    def create_user(self, m_user):
         """
-        FUUUUUUUUUUUCK!
+        input is user tweepy object
+        return result: user obj in my model, followers - list of tweepy model users, friends - also
         """
-        return [reduce(user) for user in map(what)]
 
-    def get_friends_of_user(self, user):
-        self.map_reduce(user)
+        result = user(m_user.screen_name)
+        try:
+            followers = m_user.followers()
+            friends = m_user.friends()
+            lists = m_user.lists()
+            result.followers_count = len(followers)
+            result.friends_count = len(friends)
+            result.list_count = len(lists)
+            timeline = [element.text for element in m_user.timeline()]
+            result.tweets_stat = get_statistic_of(timeline)
+            result.tweets_count = len(timeline)
+            result.friends = self.flush(friends)
+            result.followers = self.flush(followers)
+            result.lists = self.flush(lists, lambda x:x.name)
+            return {'result': result, 'followers': followers, "friends": friends, 'lists': lists}
+        except Exception as e:
+            print e
+        return None
 
-    def get_statuses_text_of_user_name(self, user):
-        string_result = ''
-        timeline = self.map_reduce(
-                {'name': 'input',
-                 'obj': self.api.get_user(user)},
-                                                map=lambda what:what['obj'].timeline(),
-                                                reduce=lambda input:{'text': input.text, 'obj': input})
-        print timeline#todo
-        return string_result
+    def scrap(self, start_user, by_what='followers'):
+        """
+        input: start_user - is tweepy user obj
+        by_what - is parameter of create_user func.
+        """
 
-bot = bot()
-#print bot.folowers
-#print bot.friends
-#print bot.timeline
-#print bot.map_reduce(bot.folowers[0])
-print bot.get_statuses_text_of_user_name('twitter')
+        user_obj = self.create_user(start_user)
+        if not user_obj:
+            return
+        #prepearing list of temp users
+        rel_users = []
+        #for followers or friends...
+        for user_related in user_obj[by_what]:
+            print 'related', user_related.screen_name
+            #flushing user in my model
+            rel_user_obj = self.create_user(user_related)
+            if not rel_user_obj:
+                continue
+            rel_users.extend(rel_user_obj[by_what])
+            self.db.save_user(rel_user_obj['result'].serialise())
+        for user in rel_users:
+            print user.screen_name
+            self.scrap(user)
+        del rel_users
+
+
+engine = engine(inited=True)
+engine.scrap(engine.api.me())
