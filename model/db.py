ce90423668b451ededdf75473330fd4e1d7fb027
Alexey Proskuryakov <alexey.proskuryakov@gmail.com>
2012-07-21 10:10:16 +0700

    some changes for db
    create difference function packages for graph visualisation and data
    base

--- model/db.py
+++ model/db.py
@@ -3,64 +3,51 @@ from pymongo.database import Database
 
 __author__ = '4ikist'
 
+#todo go out in properties!
 port = 27017
-host = '178.49.120.77'
+#host = '178.49.120.77'
+host = '127.0.0.1'
+
 db_name = 'ttr'
 users_coll_name = 'users'
 relations_coll_name = 'relations'
 entities_coll_name = 'entities'
 not_searched = 'not_searched'
+diffs = 'diffs'
+
 class db_handler():
     def __init__(self):
         self.conn = Connection(host, port)
-        self.db = Database(self.conn, db_named)
-        self.users = self.db[users_coll_name]
+        self.db = Database(self.conn, db_name)
 
+        self.users = self.db[users_coll_name]
         self.entities = self.db[entities_coll_name]
         self.relations = self.db[relations_coll_name]
         self.not_searched = self.db[not_searched]
+        self.diffs = self.db[diffs]
 
-    def get_not_searched(self):
-        return self.not_searched.find_one()
+    def get_not_searched_name(self):
+        #may be it is not good solve #-,
+        not_searched = self.not_searched.find_one()
+        self.not_searched.remove({'name': not_searched['name']})
+        return not_searched['name']
 
     def save_user(self, ser_user):
         print 'saving user: ', ser_user
         self.users.save(ser_user)
 
-    def form_nodes(self, f_name, parameter_f=lambda x: x['tweets_count']):
-        file = open(f_name, 'w+')
-        file.write("Label;Id;Weight;\n")
-        f_users = self.users.find()
-        for user in f_users:
-            file.write(user['name'] + ";" + user['name'] + ";" + str(parameter_f(user)) + "\n")
-        file.close()
-        print 'nodes formed'
+    def get_user_by_name(self, user_name):
+        return self.users.find_one({'name': user_name})
 
-
-    def form_edges(self, f_name):
-        file = open(f_name, 'w+')
-        file.write("Source;Target;Id;\n")
-        relations = set()
-        for user in self.users.find():
-            for out_ in user['friends']:
-                term = (user['name'], out_)
-                relations.add(term)
-            for in_ in user['followers']:
-                term = (in_, user['name'])
-                relations.add(term)
-        for relation in relations:
-            file.write(relation[0] + ";" + relation[1] + ";;\n")
-        print 'edges formed'
-        file.close()
-
-    #todo
-    def form_entities_edges(self, f_name):
-        entities = self.entities.find()
-        file = open(f_name, 'w+')
-        file.write("Source;Target;Id;\n")
+    def save_diffs(self, ser_diffs):
+        print 'saving diffs', ser_diffs
+        if self.users.find(
+                {'$or':
+                     [{'name': ser_diffs['one_name']},
+                             {'name': ser_diffs['two_name']}
+                     ]}).count() > 0:
+            self.diffs.save(ser_diffs)
 
 
 if __name__ == '__main__':
-    db_handler = db_handler()
-    db_handler.form_edges("d:/temp/edges.csv")
-    db_handler.form_nodes("d:/temp/nodes.csv")
+    pass
---
+++ model/graph_manager.py
@@ -0,0 +1,39 @@
+from model.db import db_handler
+
+__author__ = 'Alesha'
+class db_graph(db_handler):
+    def __init__(self):
+        self.handler = db_handler.__init__()
+
+    def form_nodes(self, f_name, parameter_f=lambda x: x['tweets_count']):
+        file = open(f_name, 'w+')
+        file.write("Label;Id;Weight;\n")
+        f_users = self.handler.users.find()
+        for user in f_users:
+            file.write(user['name'] + ";" + user['name'] + ";" + str(parameter_f(user)) + "\n")
+        file.close()
+        print 'nodes formed'
+
+
+    def form_edges(self, f_name):
+        file = open(f_name, 'w+')
+        file.write("Source;Target;Id;\n")
+        relations = set()
+        for user in self.handler.users.find():
+            for out_ in user['friends']:
+                term = (user['name'], out_)
+                relations.add(term)
+            for in_ in user['followers']:
+                term = (in_, user['name'])
+                relations.add(term)
+        for relation in relations:
+            file.write(relation[0] + ";" + relation[1] + ";;\n")
+        print 'edges formed'
+        file.close()
+
+    #todo
+    def form_entities_edges(self, f_name):
+        entities = self.handler.entities.find()
+        file = open(f_name, 'w+')
+        file.write("Source;Target;Id;\n")
+
