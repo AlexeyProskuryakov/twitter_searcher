4c647a4802283632ace8a94f38ad8d70727ba3e9
Alexey Proskuryakov <alexey.proskuryakov@gmail.com>
2013-01-15 02:15:37 +0400

    users export

--- model/graph_manager.py
+++ model/graph_manager.py
@@ -1,42 +1,33 @@
 from model.db import db_handler
-from properties import props
 
-__author__ = 'Alesha'
-class db_graph(db_handler):
+class users_export():
     def __init__(self):
-        self.handler = db_handler.__init__(props.host,props.port)
+        self.handler = db_handler(host_='localhost',port_=27107)
 
-    def form_nodes(self, f_name, parameter_f=lambda x: x['tweets_count']):
+    def form_nodes(self, f_name, parameter_f=lambda x: x['timeline_count']):
         file = open(f_name, 'w+')
         file.write("Label;Id;Weight;\n")
         f_users = self.handler.users.find()
         for user in f_users:
-            file.write(user['name'] + ";" + user['name'] + ";" + str(parameter_f(user)) + "\n")
+            file.write(user['name_'] + ";" + user['name_'] + ";" + str(parameter_f(user)) + "\n")
         file.close()
         print 'nodes formed'
 
 
-    def form_edges(self, f_name, directed = False):
+    def form_edges(self, f_name):
         file = open(f_name, 'w+')
         file.write("Source;Target;Id;Directed\n")
-
-        relations = set()
         for user in self.handler.users.find():
-            for out_ in user['friends']:
-                term = (user['name'], out_)
-                relations.add(term)
-            for in_ in user['followers']:
-                term = (in_, user['name'])
-                relations.add(term)
-
-        for relation in relations:
-            file.write(relation[0] + ";" + relation[1] + ";;\n")
+            for out_ in user['friends_relations']:
+                term = user['name_'] + ";" + out_ + ";;True\n"
+                file.write(term)
+            for in_ in user['followers_relations']:
+                term = in_+';'+user['name_'] + ";;True\n"
+                file.write(term)
         print 'edges formed'
         file.close()
 
-    #todo
-    def form_entities_edges(self, f_name):
-        entities = self.handler.entities.find()
-        file = open(f_name, 'w+')
-        file.write("Source;Target;Id;\n")
-
+if __name__ == '__main__':
+    gr = users_export()
+    gr.form_nodes('c:/temp/nodes.csv')
+    gr.form_edges('c:/temp/edges.csv')
